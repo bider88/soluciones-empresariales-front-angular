500cf5305c100b1f011f0f695aef6c36ffd195ce
Irving Didier <bider88@gmail.com>
2020-06-22 13:44:27 -0500

    valid user is logged then redirect to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/components/home/home.component';
 import { LoginComponent } from './auth/components/login/login.component';
 import { AuthGuard } from './home/guards/auth.guard';
+import { LoginGuard } from './auth/guards/login.guard';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [ AuthGuard ] },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, canActivate: [ LoginGuard ] },
   { path: '**', redirectTo: '' },
 ];
 
